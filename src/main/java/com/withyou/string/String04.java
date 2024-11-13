a15f604b5418497b0172e947c2c1a01560a95e7f
withyou <16632805736@163.com>
2024-11-13 22:23:26 +0800

    字符串第四章：实现 strStr() 函数

---
+++ src/main/java/com/withyou/string/String04.java
@@ -0,0 +1,13 @@
+package com.withyou.string;
+
+/**
+ * @author withyou
+ * @desc
+ * @create 2024/11/12 23:28
+ */
+public class String04 {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
